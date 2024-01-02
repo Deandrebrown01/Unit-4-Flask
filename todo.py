ec4f2bdc7fa50de317b1f732a773bd8ef105d51f
deandrebrown01 <dbrown@brooklynsteamcenter.org>
2024-01-02 10:50:49 -0500

    finally works

--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,redirect
 
 Goals =['Go to the gym more','Get a new outfit', 'Learn how to make a filipino dish']
 
@@ -6,7 +6,22 @@ app = Flask (__name__)
 
 @app.route('/',methods = ['GET', 'POST'])
 def Index():
-    new_todo = request.form["new_todo"]
-    Goals.append(new_todo)
+    if request.method =='POST':
+        new_todo = request.form["new_todo"]
+        Goals.append(new_todo)
+    
     return render_template("todo.html.jinja", My_Goals = Goals)
+
+
+
+@app.route('/delete_todo/<int:todo_index>',methods = ['POST'])
+def todo_delete(todo_index):
+    del Goals[todo_index]
+
+
+    return redirect('/')
+   
+
+
+
 
