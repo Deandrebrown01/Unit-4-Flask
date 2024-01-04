f69c3dcbb3e5b91a18854afade184b50019666cb
deandrebrown01 <dbrown@brooklynsteamcenter.org>
2024-01-04 10:51:44 -0500

    didn't finish finishing later

--- todo.py
+++ todo.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template,request,redirect
+import pymysql
 
 Goals =['Go to the gym more','Get a new outfit', 'Learn how to make a filipino dish']
 
@@ -9,6 +10,24 @@ def Index():
     if request.method =='POST':
         new_todo = request.form["new_todo"]
         Goals.append(new_todo)
+        con = pymysql.Connect(
+            database= Goals,
+            user="dbrown",
+            password= "228370052",
+            host="10.100.33.60",
+            cursorclass= pymysql.cursors.DictCursor
+
+        )
+        cursor = con.cursor()
+
+        cursor.execute("SELECT ``, FROM ``")
+
+        results = cursor.fetchall()
+
+
+
+        
+        
     
     return render_template("todo.html.jinja", My_Goals = Goals)
 
