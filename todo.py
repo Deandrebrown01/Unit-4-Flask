403d212333d89cf970060bbe920ad2f299c69e2a
deandrebrown01 <dbrown@brooklynsteamcenter.org>
2024-01-11 10:46:38 -0500

    finally finished with everything

--- todo.py
+++ todo.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template,request,redirect
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 import pymysql
 
 Goals =['Go to the gym more','Get a new outfit', 'Learn how to make a filipino dish']
 
 app = Flask (__name__)
+auth = HTTPBasicAuth()
 con = pymysql.Connect(
     database="dbrown_todos",
     user="dbrown",
@@ -12,7 +15,18 @@ con = pymysql.Connect(
     cursorclass= pymysql.cursors.DictCursor
 )
 
+user = {
+"De'Andre": generate_password_hash("todos")
+}
+    
+@auth.verify_password
+def verify_password(username,password):
+    if username in user and \
+    check_password_hash(user.get(username),password):
+        return username
+
 @app.route('/',methods = ['GET', 'POST'])
+@auth.login_required
 def Index():
     if request.method =='POST':
         new_todo = request.form["new_todo"]
@@ -43,9 +57,14 @@ def complete_todo(todo_index):
 
 @app.route('/delete_todo/<int:todo_index>',methods = ['POST'])
 def todo_delete(todo_index):
+    cursor = con.cursor()
 
+    cursor.execute(f"DELETE FROM `todos` WHERE `id` = {todo_index}")
 
-    return redirect('/')
+    cursor.close()
+    con.commit()
+
+    return redirect ('/')
    
 
 
